f5f6a83358c085bbe77934cd57e177f645eb7359
JulianaAraujo <juliana.gvk@gmail.com>
2020-02-06 16:59:00 -0300

    Enviando arquivo meuGrupo

---
+++ meuGrupo.js
@@ -0,0 +1,20 @@
+const pessoa = {
+  nome: 'Bruno',
+  passatempo: 'xadrez',
+  esporte: 'futebol',
+  membrosdogrupo: ['Bruno', 'Juliana', 'Denner']
+}
+
+let dadosPessoais = {
+  nome: "Denner Marchi",
+  passatempo: "Programação",
+  esporte: "Natação",
+  membrosDoGrupo: ["Bruno", "Juliana", "Denner"],
+};
+
+let dadosPessoais = [
+  Nome = "Juliana",
+  Passatempo = "ver series",
+  Esporte = "Escalada",
+  MembrosDoGrupo = ["Bruno", "Denner", "Juliana"],
+]
